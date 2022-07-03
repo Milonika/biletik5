79e3000d30388b07a5d4bc6680ba6c6e58ceb9a9
Марина Никонорова <89656135678@mail.ru>
2022-07-03 15:08:04 +0300

    Добавьте файлы проекта.

---
+++ biletik5/Data/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace biletik5.Data
+{
+    public class User
+    {
+        public User(string name, string surame, DateTime dateOfAdding)
+        {
+            Name = name;
+            Surame = surame;
+            DateOfAdding = dateOfAdding;
+        }
+
+        public string Name { get; set; }
+        public string Surame { get; set; }
+        public DateTime DateOfAdding { get; set; }
+
+        public static List<User> users = new List<User>();
+        public static List<string> title = new List<string>() {"Name","Surname", "DateOfAdding"};
+
+    }
+}
